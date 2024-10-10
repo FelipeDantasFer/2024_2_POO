83eddd8cffd575f2298b09972982354e41792196
Felipe Fernandes <110199684+FelipeDantasFer@users.noreply.github.com>
2024-10-10 07:44:17 -0300

    Create ex01.py

---
+++ Aula_03/ex01.py
@@ -0,0 +1,13 @@
+x = 5
+y = 5.5
+z = false
+s = "TADS - Tecnologia em Análise e Desenvolvimento de Sistemas"
+l = s.split()
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+print(s, type(s))
+print(l)
+print(type(l))
+l.sort()
+print(l)
