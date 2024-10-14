d99308cb9bb41134f8192650f7b3495e1e004003
Felipe Fernandes <110199684+FelipeDantasFer@users.noreply.github.com>
2024-10-14 20:59:46 +0000

    Consumo Medio

---
+++ AtividadesPoo/Consumo.py
@@ -0,0 +1,4 @@
+x = int(input("Distância total percorrida: "))
+y = float(input("Total de combustível gasto: "))
+consumoMedio = (x / y)
+print(f"Consumo médio do automóvel em km/l foi de : {consumoMedio:.3f}. ")
